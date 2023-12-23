01401a6bbafdf6c85e77fe8f5d0b575a4fc58038
nodejavascript <nodejsjavascript@gmail.com>
2023-12-23 12:05:25 -0500

    added magnetics sensor and an indicator (white) led

--- README.md
+++ README.md
@@ -1,20 +1,27 @@
 # wemos-pir-to-mqtt
 
-This Arduino-based project utilizes the LOLIN(WEMOS) D1 mini (clone) board to monitor Wi-Fi and MQTT connections through LED indicators. Additionally, a passive infrared (PIR) sensor detects state changes.
+This Arduino-based project utilizes the LOLIN(WEMOS) D1 mini (clone) board to monitor Wi-Fi and MQTT connections through LED indicators. Additionally, a passive infrared (PIR) sensor detects state changes and detects if the door is opened.
 
 ![LOLIN(WEMOS) D1 mini Board](./picture.jpg)
 
-*Image of my installation attached to the side of my monitor with LEDs and PIR (HC-SR505)*
+_Image of my installation attached to the side of my monitor with LEDs and PIR (HC-SR505)_
 
 ## Features
 
 - **LED Indicators:**
+
   - Red (PIN D8): Indicates disconnected Wi-Fi or MQTT. (~ 1.5k ohms resistor)
   - Blue (PIN D5): Denotes Wi-Fi connection status. (~ 2.7k ohms resistor)
   - Green (PIN D7): Represents MQTT connection status. (~ 800 ohms resistor)
+  - White (PIN D6): Represents Door open / closed status. (~ 2.2k ohms resistor)
 
 - **PIR Sensor:**
-  - Connected to PIN D6. Part: HC-SR505
+
+  - Connected to PIN D2. Part: HC-SR505
+
+- **Door magnetic Sensor:**
+
+  - Connected to PIN D1.
 
 - **MQTT Communication:**
   - Sends MQTT messages every 10 seconds regardless of state changes.
@@ -25,6 +32,7 @@ This Arduino-based project utilizes the LOLIN(WEMOS) D1 mini (clone) board to mo
 1. Update `config.h` with your WIFI and MQTT credentials. (copy config.h.example to config.h)
 2. Upload the code to the LOLIN(WEMOS) D1 mini board.
 3. Connect LEDs to their respective pins as specified.
-4. Connect the PIR sensor to PIN D6.
-5. Observe LED indicators for Wi-Fi and MQTT statuses.
-6. Check the serial monitor for additional debug information.
+4. Connect the PIR sensor to PIN D2.
+5. Connect the DOOR sensor to PIN D1.
+6. Observe LED indicators for Wi-Fi and MQTT statuses.
+7. Check the serial monitor for additional debug information.
--- wemos-pir-to-mqtt.ino
+++ wemos-pir-to-mqtt.ino
@@ -3,31 +3,38 @@
 // red is on if either wifi on mqtt is not connected
 // blue is wifi connection
 // green is mqtt connection
+// white is the indicator, goes high if motion is detected
 // mqtt is sent every 10 seconds regardless
 // mqtt is sent every state change too
+// door pin uses magnetic sensor for door opening
 
 #include "config.h"
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
-// Replace with your network credentials
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 
-// Replace with your MQTT credentials
 const char* mqtt_server = MQTT_SERVER;
 const char* mqtt_user = MQTT_USER;
 const char* mqtt_password = MQTT_PASSWORD;
 const char* mqtt_client_name = MQTT_CLIENT_NAME;
 
+
+StaticJsonDocument<200> doc;
+String payload;
+
 int delayMillis = 10000;
 int millisNow = 0;
 
-int prevState = 2;
+int doorPrevState = 2;
+int pirPrevState = 2;
 
+#define DOOR_PIN D1
 #define PIR_PIN D2
 #define WIFI_PIN D5
+#define INDICATOR_PIN D6
 #define MQTT_PIN D7
 #define ERROR_PIN D8
 
@@ -40,17 +47,21 @@ void setup() {
 
   Serial.print("MQTT_SERVER ");
   Serial.println(MQTT_SERVER);
-  
+
   client.setServer(mqtt_server, 1883);
+
+  pinMode(DOOR_PIN, INPUT_PULLUP);
   pinMode(PIR_PIN, INPUT);
   pinMode(WIFI_PIN, OUTPUT);
+  pinMode(INDICATOR_PIN, OUTPUT);
   pinMode(MQTT_PIN, OUTPUT);
   pinMode(ERROR_PIN, OUTPUT);
 
   digitalWrite(WIFI_PIN, LOW);
+  digitalWrite(INDICATOR_PIN, LOW);
   digitalWrite(MQTT_PIN, LOW);
   digitalWrite(ERROR_PIN, HIGH);
-  
+
   delay(250);
   setup_wifi();
   delay(1000);
@@ -63,77 +74,96 @@ void loop() {
     digitalWrite(ERROR_PIN, LOW);
   }
 
-  
+
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 
-  int state = digitalRead(PIR_PIN);
-  Serial.print("state ");
-  Serial.println(state);
-  Serial.print("prevState ");
-  Serial.println(prevState);
+  int doorState = digitalRead(DOOR_PIN);
+  String doorTopic = String(MQTT_TOPIC) + "/DOOR";
 
-  String full_topic = String(mqtt_client_name) + "/PIR";
-  String state_value = String(state);
+  int pirState = digitalRead(PIR_PIN);
+  String pirTopic = String(MQTT_TOPIC) + "/PIR";
 
-
-
-
-  if (state != prevState) {
-    Serial.println("state send");
-    
-    // Create a JSON object
-    StaticJsonDocument<200> doc;
-    doc["type"] = "state";
-    doc["PIR"] = state;
-    String payload;
-    serializeJson(doc, payload);
-    client.publish(full_topic.c_str(), payload.c_str());
+  if (pirState == 1) {
+    digitalWrite(INDICATOR_PIN, HIGH);
+  } else {
+    digitalWrite(INDICATOR_PIN, LOW);
   }
 
-  prevState = state;
+  int published = 0;
+
+  if (doorState != doorPrevState) {
+    published = 1;
+    client.publish(doorTopic.c_str(), returnJsonPayload("state", "DOOR", doorState).c_str());
+    Serial.println("doorState: " + String(doorState) + ", doorPrevState: " + String(doorPrevState));
+  }
+
+  if (pirState != pirPrevState) {
+    published = 1;
+    client.publish(pirTopic.c_str(), returnJsonPayload("state", "PIR", pirState).c_str());
+    Serial.println("pirState: " + String(pirState) + ", pirPrevState: " + String(pirPrevState));
+  }
+
 
   if (millisNow >= delayMillis) {
-    Serial.println("reg send");
+    published = 1;
     millisNow = 0;
-
-    // Create a JSON object
-    StaticJsonDocument<200> doc;
-    doc["type"] = "interval";
-    doc["PIR"] = state;
-    String payload;
-    serializeJson(doc, payload);
-    client.publish(full_topic.c_str(), payload.c_str());
+    client.publish(doorTopic.c_str(), returnJsonPayload("interval", "DOOR", doorState).c_str());
+    delay(100);
+    client.publish(pirTopic.c_str(), returnJsonPayload("interval", "PIR", pirState).c_str());
   }
-  
-  
-  delay(1000);
+
+
+  doorPrevState = doorState;
+  pirPrevState = pirState;
+
+  if (published == 1) {
+    digitalWrite(ERROR_PIN, HIGH);
+    delay(250);
+    digitalWrite(ERROR_PIN, LOW);
+    delay(750);
+  } else {
+    delay(1000);
+  }
+
   millisNow += 1000;
+  Serial.println("millisNow: " + String(millisNow));
+
+}
+
+
+String returnJsonPayload(String type, String key, int payload) {
+  StaticJsonDocument<256> doc;
+  doc["type"] = type;
+  doc[key] = String(payload);
+
+  String payloadSerialized;
+  serializeJson(doc, payloadSerialized);
+  return payloadSerialized;
 }
 
 void setup_wifi() {
-
-  delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
+
   WiFi.hostname("PIR");
 
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     digitalWrite(WIFI_PIN, LOW);
-    delay(500);
-    delay(250);
+    delay(300);
+    digitalWrite(WIFI_PIN, HIGH);
+    delay(300);
     Serial.print(".");
   }
 
   digitalWrite(WIFI_PIN, HIGH);
-  
+
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -145,23 +175,26 @@ void reconnect() {
   digitalWrite(MQTT_PIN, LOW);
   // Loop until we're reconnected
   while (!client.connected()) {
-    
+
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
     if (client.connect(mqtt_client_name, mqtt_user, mqtt_password)) {
       digitalWrite(MQTT_PIN, HIGH);
-        
+
       Serial.println("connected");
 
-      String full_topic = String(mqtt_client_name) + "/CONNECTED";
+      String full_topic = String(MQTT_TOPIC) + "/CONNECTED";
       client.publish(full_topic.c_str(), "1");
-      
+
     } else {
+
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+      digitalWrite(MQTT_PIN, HIGH);
+      delay(2500);
+      digitalWrite(MQTT_PIN, LOW);
+      delay(2500);
     }
   }
 }
